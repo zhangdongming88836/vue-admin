7bba8b59d7cc98664416795d4b7a5f3880cb7b9c
zhangdongming <554332367@qq.com>
2020-07-28 21:43:19 +0800

    构建项目。配置全局样式，路由。element-ui

---
+++ src/views/login/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="login">
+        这里是登录页
+        <el-button>默认按钮</el-button>
+    </div>
+</template>
+<script>
+export default {
+    name:"login"
+}
+</script>
+<style lang="scss" scoped>
+#login{
+    height:50vh;
+    background-color:#344a5f;
+}
+</style>
