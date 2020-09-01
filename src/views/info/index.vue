5765a0b23ad78d73c56e80acb804c88916de2ad6
zhangdongming <554332367@qq.com>
2020-09-01 18:10:50 +0800

    添加编辑详情

--- src/views/info/index.vue
+++ src/views/info/index.vue
@@ -63,6 +63,7 @@
         <template slot-scope="scope">
           <el-button type="danger" size="mini" @click="deleteItem(scope.row.id)">删除</el-button>
           <el-button type="success" size="mini" @click="editInfo(scope.row.id)">编辑</el-button>
+          <el-button type="success" size="mini" @click="editInfo(scope.row.id)">编辑详情</el-button>
         </template>
       </el-table-column>
     </el-table>
